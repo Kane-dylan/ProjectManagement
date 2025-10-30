a7d91224d3cb3edef5190ab4c5ece914d565bf82
Kane Dylan <cool.kdtech1234@gmail.com>
2025-10-30 20:02:27 +0530

    port exp

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import dotenv from "dotenv"
-import express from "express"
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
 })
 
-const app = express();
 const port =process.env.PORT || 3000;
 
-app.get("/", (req,res) => {
-  res.send("Hello");
-})
-
-app.get("/social", (req, res) =>{ 
-  res.send("We have no socials at this moment")
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on  http://localhost:${port}`);
