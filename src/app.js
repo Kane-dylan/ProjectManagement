e15a537bde7159f651330a6f1d92ac752531114e
Kane Dylan <cool.kdtech1234@gmail.com>
2025-11-06 20:34:56 +0530

    error handle

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
+// basic configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// cors config
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "https://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.get("/", (req, res) => {
-  res.send("Welcome to basecamp")
-})
+  res.send("Welcome to My Manager");
+});
 
 export default app;
